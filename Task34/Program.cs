5ea76dd274fbc52a47cd06c70135229b0b60da59
AlexandrSawrasov <caiiiuk@mail.ru>
2022-11-14 19:52:07 +0300

    add homework Task34/36/38

---
+++ Task34/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//            Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetRandArr(int lenght, int begin, int end)
+{
+    int[] result = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        result[i] = new Random().Next(begin, end + 1);
+    }
+    return result;
+}
+
+int[] array = GetRandArr(333, 100, 999);
+Console.WriteLine($"{String.Join(", ", array)}");
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0) count++;
+}
+Console.WriteLine($"Количество четных чисел в массиве: {count}");
---
+++ Task38/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+//Глядя на пример, осмелюсь предположить, что все же имелись ввиду натуральные числа...
+
+int[] GetRandArr(int lenght, int begin, int end)
+{
+    int[] result = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        result[i] = new Random().Next(begin, end + 1);
+    }
+    return result;
+}
+
+int[] array = GetRandArr(333, 1, 9999);
+Console.WriteLine($"{String.Join(", ", array)}");
+int min = array[0];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min) min = array[i];
+    else if (array[i] > max) max = array[i];
+}
+Console.WriteLine($"Разница меж максимальным и минимальным элементом массива: {max - min}");
